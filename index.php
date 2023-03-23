4ee286318b255703d5504d3d93896dc4e7ec9ace
FreddyDGL <fgonzalezluna0@gmail.com>
2023-03-22 18:53:08 -0600

    login

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<?php include("template/header.php");?>
+<?php include("template/header.php"); ?>
 <section>
-        <div class=" contMain">
-            <h1>Paint Information</h1>
-            <form action="">
-                <div class="contUser">
-                    <div class="row justify-content-evenly text-center">
-                        <a href="nEmpresa.php" target="">
-                            Registar Empresa
-                        </a>
-                        <a href="login.php">
-                            Iniciar Sesión
-                        </a>
-                    </div>
+    <div class=" contMain">
+        <h1>Paint Information</h1>
+        <form action="">
+            <div class="contUser">
+                <div class="row justify-content-evenly text-center">
+                    <a href="nEmpresa.php" target="">
+                        Registar Empresa
+                    </a>
+                    <a href="login.php">
+                        Iniciar Sesión
+                    </a>
                 </div>
-            </form>
-        </div>
-    </section>
+            </div>
+        </form>
+    </div>
+</section>
 <?php include("template/footer.php"); ?>
--- login.php
+++ login.php
@@ -1,3 +1,30 @@
-<?php include("template/header.php");?>
+<?php include("template/header.php"); ?>
 
+<section>
+    <div class="contMain">
+        <h1>Paint Information</h1>
+        <form action="">
+            <div class="card cardLogin contUser">
+                <div class="card-header">
+                    <h2>Login</h2>
+                </div>
+                <div class="card-body  ">
+                    <div class="form-group">
+                        <label for="exampleInputEmail">Usuario</label>
+                        <input type="text" class="form-control" name="usuario" aria-describedby="emailHelp"
+                            placeholder="Ingresa el usuario">
+                        <small class="form-text text-muted">Ingresa el usuario con el te registraste</small>
+                    </div>
+                    <div class="form-group ">
+                        <label for="exampleInputEmail">Contraseña</label>
+                        <input type="password" class="form-control" name="contrasenia" aria-describedby="emailHelp"
+                            placeholder="Ingresa tu Contraseña">
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary"> Ingresar </button>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
 <?php include("template/footer.php"); ?>
